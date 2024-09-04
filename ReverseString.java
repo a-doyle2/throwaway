782e92148db3a12245476fd168cd467330c7aeee
Andrew Doyle <106994280+a-doyle2@users.noreply.github.com>
2024-09-04 19:28:44 +0000

    reverse

---
+++ ReverseString.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = scanner.nextLine();
+        s = reverse(s.toCharArray());
+        System.out.println("Reversed: "+s);
+    }
+
+    public static String reverse(char[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+}
