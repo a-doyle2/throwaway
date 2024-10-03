da1ee6a5e9fe282746c4e790d9759bb4f552f393
Andrew Doyle <106994280+a-doyle2@users.noreply.github.com>
2024-10-03 19:19:23 +0000

    a

---
+++ Sets.java
@@ -0,0 +1,10 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sets {
+    public static void main(String[] args) {
+        Set<Double> s = new TreeSet<Double>();
+        s.add(234);
+        System.out.println(s);
+    }
+}
